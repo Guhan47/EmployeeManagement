09440c3ec7729ed474c05d7f56b54c63d4994c1c
Guhan47 <gooksguha47@gmail.com>
2025-09-07 23:54:31 +0530

    latest update

--- Backend/Express/server.js
+++ Backend/Express/server.js
@@ -7,7 +7,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use("/uploads", express.static(path.join(__dirname, "uploads")))
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")))
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
